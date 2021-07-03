8535cd593e8db4f404fd2dbe594fbe11d65dfe5c
Kaushik Mazumdar <kmzdr1@gmail.com>
2021-07-03 06:05:09 +0530

    Doc update: Data inserted in new table

---
+++ database1.sql
@@ -0,0 +1,21 @@
+INSERT INTO worker_table VALUES(1,'Anand','Issac','Marketing', 40000, 'ananac@ineuron.com' , '2015-04-22');
+INSERT INTO worker_table VALUES(2,'Aayushi','Natarajan', 'Purchase', 35000, 'aayushan@gmail.com' , '2015-05-14'),
+(3,'Amitabh','Thej', 'Marketing', 10000, 'Amitabhthj@ineauron.com' , '2006-03-17'),
+	(4,'Kajol','Behl', 'Finance', 45000, 'kajolhl@ineauron.com' , '2015-09-30'),
+	(5,'Heena','Mangat', 'HR', 50000, 'heenamgat@ineauron.com' , '2016-12-10'),
+	(6,'Vipul','Kapoor', 'Purchase', 30000, 'Vipulk@ineauron.com' , '2015-07-11'),
+	(7,'Mohit ','Bahl', 'Finance', 50000, 'mohitbhl@gmail.com' , '2017-02-16'),
+	(8,'Pranay ', 'Rastogi', 'Marketing', 45000, 'pranagi@ineauron.com' , '2014-02-12'),
+	(9,'Sathish','Bhat', 'HR', 25000, 'Sathishbht@ineauron.com' , '2017-05-13'),
+	(10,'Jatin', 'Gaba', 'Purchase', 45000, 'jatiaba@ineauron.com' , '2016-08-12'),
+	(11,'Anil', 'Gobin', 'Finance', 30000, 'anilgoin@ineauron.com' , '2014-02-18'),
+	(12,'Marlo', 'Sarin', 'Purchase', 25000, 'marlosrin@ineauron.com' , '2017-05-13'),
+	(13,'Kajol','Behl', 'Finance', 45000, 'kajolhl@ineauron.com' , '2015-09-30');
+   
+    
+   	INSERT INTO worker_table VALUES(14,"Kaushik",'Kumar', 'HR', 45000, 'kaushik@ineuron.com', '2017-09-23'),
+    (15,'shatabdi', 'Roy', 'Product', 39000, 'sakshiroy@gmail.com',' 2016-01-31');
+    
+    iNSERT INTO worker_table VALUES(16,"Vipul",'Kumar', 'Product', 50000, 'Vipul@ineuron.com', '2015-09-23');
+    Insert into worker_table values(17,'Satish','sharma', 'Finance', 38000, 'sharmasatish@ineauron.com' , '2016-09-18');
+    
